7859757d8810063559a7e0d95ffbd22b5a0164c6
Albertina Serna Sabedra <labettys@gmail.com>
2009-06-11 20:55:17 -0500

    Scaffold para productos y categorias

---
+++ db/migrate/20090612015328_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration
+  def self.up
+    create_table :products do |t|
+      t.integer :category_id
+      t.string :name
+      t.string :permalink
+      t.integer :position
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :products
+  end
+end
